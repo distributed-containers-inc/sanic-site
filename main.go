18936b4d42670c657941a254b3fb5970553a5f3e
Colin Chartier <colin.n.chartier@gmail.com>
2019-05-19 18:26:24 -0400

    First commit: Set up Gin

---
+++ main.go
@@ -0,0 +1,24 @@
+package main
+
+import (
+	"net/http"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+	router.Delims("{{", "}}")
+	router.LoadHTMLFiles("./templates/index.tmpl")
+
+	router.GET("/", func(c *gin.Context) {
+		c.Redirect(http.StatusMovedPermanently, "index.html")
+	})
+	router.GET("/index.html", func(c *gin.Context) {
+		c.HTML(http.StatusOK, "index.tmpl", map[string]interface{}{
+			"Test": "hullo!",
+		})
+	})
+
+	router.Run("0.0.0.0:8080")
+}
