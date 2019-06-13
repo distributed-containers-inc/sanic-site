84ec61a4963d93fd7a8ded4307aec317e6e13dea
Colin Chartier <colin.n.chartier@gmail.com>
2019-06-12 21:41:39 -0400

    Add terminal to index

--- app/common/carousel.js
+++ app/common/carousel.js
@@ -1,35 +1,10 @@
 import React from 'react';
-import { makeStyles, useTheme } from '@material-ui/core/styles';
-import MobileStepper from '@material-ui/core/MobileStepper';
 import Button from '@material-ui/core/Button';
 import KeyboardArrowLeft from '@material-ui/icons/KeyboardArrowLeft';
 import KeyboardArrowRight from '@material-ui/icons/KeyboardArrowRight';
 import SwipeableViews from 'react-swipeable-views';
 
-const useStyles = makeStyles(theme => ({
-    root: {
-        maxWidth: 400,
-        flexGrow: 1,
-    },
-    header: {
-        display: 'flex',
-        alignItems: 'center',
-        height: 50,
-        paddingLeft: theme.spacing(4),
-        backgroundColor: theme.palette.background.default,
-    },
-    img: {
-        height: 255,
-        display: 'block',
-        maxWidth: 400,
-        overflow: 'hidden',
-        width: '100%',
-    },
-}));
-
 function SwipeableTextMobileStepper(props) {
-    const classes = useStyles();
-    const theme = useTheme();
     const [activeStep, setActiveStep] = React.useState(0);
     const maxSteps = props.panels.length;
 
@@ -46,33 +21,23 @@ function SwipeableTextMobileStepper(props) {
     }
 
     return (
-        <div className={classes.root}>
+        <div>
+            <Button size="small" onClick={handleNext}
+                    disabled={activeStep === maxSteps - 1}>
+                <KeyboardArrowLeft/> Next
+            </Button>
             <SwipeableViews
-                axis={theme.direction === 'rtl' ? 'x-reverse' : 'x'}
+                axis='x'
                 index={activeStep}
                 onChangeIndex={handleStepChange}
                 enableMouseEvents
             >
                 {props.panels.map((panel, idx) => <div key={idx}>{panel}</div>)}
             </SwipeableViews>
-            <MobileStepper
-                variant="dots"
-                steps={maxSteps}
-                position="static"
-                activeStep={activeStep}
-                nextButton={
-                    <Button size="small" onClick={handleNext} disabled={activeStep === maxSteps - 1}>
-                        Next
-                        {theme.direction === 'rtl' ? <KeyboardArrowLeft /> : <KeyboardArrowRight />}
-                    </Button>
-                }
-                backButton={
-                    <Button size="small" onClick={handleBack} disabled={activeStep === 0}>
-                        {theme.direction === 'rtl' ? <KeyboardArrowRight /> : <KeyboardArrowLeft />}
-                        Back
-                    </Button>
-                }
-            />
+            <Button size="small" onClick={handleBack}
+                    disabled={activeStep === 0}>
+                Back <KeyboardArrowLeft/>
+            </Button>
         </div>
     );
 }
--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -1,17 +1,51 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Carousel from '../../common/carousel';
+import {createMuiTheme} from '@material-ui/core/styles';
+import ReactTerminal from 'react-terminal-component';
+import ThemeProvider from "@material-ui/styles/ThemeProvider/ThemeProvider";
+
+const theme = createMuiTheme({
+    palette: {
+        type: 'dark',
+        primary: {
+            main: '#acc1cf',
+        },
+        secondary: {
+            main: '#2c2e33',
+        },
+    }
+                             });
 
 function SanicFeatures() {
     return (
-        <div className="content">
-        <Carousel variant="contained" color="primary" panels={[
-            <div>panel 1</div>,
-            <div>panel 2</div>,
-            <div>panel 3</div>,
-        ]}/>
-        </div>
+        <ThemeProvider theme={theme}>
+            <div className="content column">
+                <Carousel variant="contained" color="primary" panels={[
+                    <h2>A rich environment system</h2>,
+                    <div>panel 2</div>,
+                    <div>panel 3</div>,
+                ]}/>
+                <ReactTerminal
+                    autoFocus={false}
+                    clickToFocus={true}
+                    theme={{
+                        background: '#18181d',
+                        promptSymbolColor: '#acc1cf',
+                        commandColor: '#acc1cf',
+                        outputColor: '#acc1cf',
+                        errorOutputColor: '#ff4a4a',
+                        fontSize: '1.1rem',
+                        spacing: '1%',
+                        fontFamily: 'monospace',
+                        width: '40em',
+                        height: '6em',
+                    }}
+                />
+            </div>
+        </ThemeProvider>
     );
 }
 
-ReactDOM.render(<SanicFeatures />, document.querySelector('#sanic-features-interactive'));
+ReactDOM.render(<SanicFeatures/>,
+    document.querySelector('#sanic-features-interactive'));
