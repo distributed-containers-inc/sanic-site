379cbe6afd49b432935cc30b8903b96b4af724f3
Colin Chartier <colin.n.chartier@gmail.com>
2019-06-13 15:23:18 -0400

    Add force graph to index environments

--- app/pages/download/download.js
+++ app/pages/download/download.js
@@ -1,13 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import Button from '@material-ui/core/Button';
 
 function Download() {
     return (
         <div className="content">
-        <Button variant="contained" color="primary">
             Download: Hello World
-        </Button>
         </div>
     );
 }
--- app/pages/index/index.js
+++ app/pages/index/index.js
@@ -3,15 +3,50 @@ import ReactDOM from 'react-dom';
 import Carousel from '../../common/carousel';
 import palette from '../../common/palette';
 import ReactTerminal from 'react-terminal-component';
+import ForceGraph2D from 'react-force-graph-2d';
+
+class ForceGraphEnvironment extends React.Component {
+    constructor(props) {
+        super(props);
+        this.forceGraph = React.createRef();
+    }
+
+    componentDidMount() {
+        this.forceGraph.current.zoom(1)
+    }
+
+    render() {
+        return (
+            <div style={{display: 'flex', flexDirection: 'row'}}>
+                <ForceGraph2D
+                    ref={this.forceGraph}
+                    linkDirectionalParticles={2}
+                    graphData={{
+                        nodes: [{id: 1}, {id: 2}],
+                        links: [{source: 1, target: 2}]
+                    }}
+                    width={150}
+                    height={150}
+                />
+                <span style={{marginTop: 'auto', marginBottom: 'auto'}}>
+                {this.props.name}
+                </span>
+            </div>
+        )
+    }
+}
 
 function SanicFeatures() {
     return (
         <div style={{display: 'flex', flexDirection: 'row'}}>
-            <div style={{display: 'flex', flexDirection: 'column'}}>
+            <div style={{display: 'flex', flexDirection: 'column', width: '75%'}}>
                 <Carousel variant="contained" color="primary" panels={[
-                    <h2 style={{textAlign: 'center'}}>A rich environment system</h2>,
-                    <h2 style={{textAlign: 'center'}}>A rich environment system</h2>,
-                    <h2 style={{textAlign: 'center'}}>A rich environment system</h2>,
+                    <h2 style={{textAlign: 'center'}}>A rich environment
+                        system</h2>,
+                    <h2 style={{textAlign: 'center'}}>A rich environment
+                        system</h2>,
+                    <h2 style={{textAlign: 'center'}}>A rich environment
+                        system</h2>,
                 ]}/>
                 <ReactTerminal
                     autoFocus={false}
@@ -25,7 +60,7 @@ function SanicFeatures() {
                         fontSize: '1.1rem',
                         spacing: '1%',
                         fontFamily: 'monospace',
-                        width: 'inherit',
+                        width: '98%',
                         height: '6em',
                     }}
                 />
@@ -33,9 +68,11 @@ function SanicFeatures() {
             <div style={{
                 display: 'flex',
                 flexDirection: 'column',
-                flex: '0 1 auto'
+                width: '25%'
             }}>
-                dev<br />staging<br />prod<br />
+                <ForceGraphEnvironment name='dev' style={{marginTop: 'auto', marginBottom: 'auto'}}/>
+                <ForceGraphEnvironment name='staging' style={{marginTop: 'auto', marginBottom: 'auto'}}/>
+                <ForceGraphEnvironment name='prod' style={{marginTop: 'auto', marginBottom: 'auto'}}/>
             </div>
         </div>
     );
