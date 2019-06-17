3b57157fc6564ebc909aeb8143fbd0e17aba65e2
Colin Chartier <colin.n.chartier@gmail.com>
2019-06-17 14:14:49 -0400

    Convert carousel to a class

--- app/common/carousel.js
+++ app/common/carousel.js
@@ -2,54 +2,63 @@ import React from 'react';
 import SwipeableViews from 'react-swipeable-views';
 import palette from './palette';
 
-function SwipeableTextMobileStepper(props) {
-    const [activeStep, setActiveStep] = React.useState(0);
-    const maxSteps = props.children.length;
-
-    function handleNext() {
-        setActiveStep(prevActiveStep => prevActiveStep + 1);
+class SwipeableTextMobileStepper extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            step: 0
+        }
     }
 
-    function handleBack() {
-        setActiveStep(prevActiveStep => prevActiveStep - 1);
+    handleNext() {
+        this.setState(prevState => ({step: prevState.step+1}));
     }
 
-    function handleStepChange(step) {
-        setActiveStep(step);
+    handleBack() {
+        this.setState(prevState => ({step: prevState.step-1}));
     }
 
-    return (
-        <div style={{display: 'flex', flexDirection: 'row'}}>
-            <button onClick={handleBack}
-                    disabled={activeStep === 0}
-                    style={{
-                        backgroundColor: palette.primary.background.main,
-                        color: palette.primary.text.main,
-                        border: '0'
-                    }}>
-                &lt;
-            </button>
-            <SwipeableViews
-                axis='x'
-                index={activeStep}
-                onChangeIndex={handleStepChange}
-                enableMouseEvents
-                style={{width: '100%'}}
-                animateHeight={true}
-            >
-                {props.children}
-            </SwipeableViews>
-            <button onClick={handleNext}
-                    disabled={activeStep === maxSteps - 1}
-                    style={{
-                        backgroundColor: palette.primary.background.main,
-                        color: palette.primary.text.main,
-                        border: '0'
-                    }}>
-                &gt;
-            </button>
-        </div>
-    );
+    handleStepChange(step) {
+        this.setState({step: step});
+        if(this.props.onChangeIndex) {
+            this.props.onChangeIndex.call(arguments);
+        }
+    }
+
+    render() {
+        return (
+            <div style={{display: 'flex', flexDirection: 'row'}}>
+                <button onClick={() => this.handleBack()}
+                        disabled={this.state.step === 0}
+                        style={{
+                            backgroundColor: palette.primary.background.main,
+                            color: palette.primary.text.main,
+                            border: '0'
+                        }}>
+                    &lt;
+                </button>
+                <SwipeableViews
+                    axis='x'
+                    index={this.state.step}
+                    onChangeIndex={() => this.handleStepChange}
+                    enableMouseEvents
+                    style={{width: '100%'}}
+                    animateHeight={true}
+                >
+                    {this.props.children}
+                </SwipeableViews>
+                <button onClick={() => this.handleNext()}
+                        disabled={this.state.step === this.props.children.length}
+                        style={{
+                            backgroundColor: palette.primary.background.main,
+                            color: palette.primary.text.main,
+                            border: '0'
+                        }}>
+                    &gt;
+                </button>
+            </div>
+        );
+    }
 }
 
 export default SwipeableTextMobileStepper;
