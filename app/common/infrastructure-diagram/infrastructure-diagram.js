9e9cee15ea6cd66bf6c1211ab0ea8cd1e53e3511
Colin Chartier <colin.n.chartier@gmail.com>
2019-06-16 23:46:14 -0400

    Add anchoring forces to index page nodes

--- app/common/infrastructure-diagram/infrastructure-diagram.js
+++ app/common/infrastructure-diagram/infrastructure-diagram.js
@@ -12,9 +12,10 @@ class InfrastructureDiagram extends React.Component {
     generateGraphData() {
         return {
             nodes: [
-                {id: 1, name: "node 1"},
+                {id: 1, name: "Developer PC", position: 'top-top-center'},
                 {id: 2, name: "node 2"},
-                {id: 3, name: "node 3"}],
+                {id: 3, name: "node 3"},
+                {id: 4, name: "Registry", position: "center-left"}],
             links: [
                 {source: 1, target: 2},
                 {source: 3, target: 2}]
@@ -28,17 +29,55 @@ class InfrastructureDiagram extends React.Component {
                       })
     }
 
+    forceCenterPositioned() {
+        let nodes;
+        let diagram = this;
+
+        const getPositions = () => {
+            return {
+                "top-top-center": {
+                    x: 0,
+                    y: -0.18*diagram.state.height
+                },
+                "center": {
+                    x: 0,
+                    y: 0
+                },
+                "center-left": {
+                    x: -0.18*diagram.state.width,
+                    y: 0
+                },
+            }
+        };
+
+        function force(alpha) {
+            let positions = getPositions();
+            for (let node of nodes) {
+                let pos = positions[node.position];
+                if(pos) {
+                    node.vx += (pos.x - node.x) * alpha;
+                    node.vy += (pos.y - node.y) * alpha;
+                }
+            }
+        }
+
+        force.initialize = function (_) {
+            nodes = _;
+        };
+
+        return force;
+    }
+
     componentDidMount() {
         this.updateDimensions();
-        this.containerRef.current.addEventListener("resize", this.updateDimensions)
+        this.graphRef.current.d3Force("center", this.forceCenterPositioned());
     }
 
     componentWillUnmount() {
-        this.containerRef.current.removeEventListener("resize", this.updateDimensions)
     }
 
     render() {
-        const canvas =  (
+        const canvas = (
             <ForceGraph2D
                 ref={this.graphRef}
                 width={this.state && this.state.width || window.outerWidth}
@@ -51,10 +90,20 @@ class InfrastructureDiagram extends React.Component {
 
         return (
             <div ref={this.containerRef}
-                 style={{width: '100%', height: '30em', backgroundColor: palette.primary.background.main, content: 'border-box', paddingTop: '1em'}}>
+                 style={{
+                     width: '100%',
+                     height: '30em',
+                     backgroundColor: palette.primary.background.main,
+                     content: 'border-box',
+                     paddingTop: '1em'
+                 }}>
                 {this.props.title ? (
-                    <h2 style={{textAlign: 'center', width: '100%', margin: '0'}}>{this.props.title}</h2>
-                ): null}
+                    <h2 style={{
+                        textAlign: 'center',
+                        width: '100%',
+                        margin: '0'
+                    }}>{this.props.title}</h2>
+                ) : null}
                 {canvas}
             </div>
         )
